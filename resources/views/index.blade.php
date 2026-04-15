4fe0ebd82698b8f83e1cef26c5c929095b4376a3
Ahmed Rabie <arabeea7104@gmail.com>
2026-04-16 00:59:11 +0200

    Checkpoint from VS Code for cloud agent session

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+@extends('nav')
+
+@section('PageTitle', 'Home Page')
+
+@section('Content')
+    <section class=" mt-8 overflow-hidden bg-gray-50 sm:grid sm:grid-cols-2 sm:items-center">
+        <div class="p-8 md:p-12 lg:px-16 lg:py-24">
+            <div class="mx-auto max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+                <h2 class="text-2xl font-bold text-gray-900 md:text-3xl">
+                    Welcome to ITI Blog Homepage!
+                </h2>
+
+                <p class="hidden text-gray-500 md:mt-4 md:block">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam
+                    sed. Quam a scelerisque amet ullamcorper eu enim et fermentum, augue. Aliquet amet
+                    volutpat quisque ut interdum tincidunt duis.
+                </p>
+
+
+            @endsection
