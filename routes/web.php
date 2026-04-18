ee559eeb9b7d65bf3a7d82cfed9dcb409485c376
Ahmed Rabie <arabeea7104@gmail.com>
2026-04-18 15:05:13 +0200

    feat: Implement authentication and user module using breeze.

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,27 @@
 <?php
 
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Models\Post;
 
+
+
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create')->middleware("auth");
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 Route::get('/posts/{uuid}/edit', [PostController::class, 'edit'])->name('posts.edit');
 Route::put('/posts/{uuid}', [PostController::class, 'update'])->name('posts.update');
@@ -18,3 +30,5 @@ Route::patch('/posts/{uuid}/restore', [PostController::class, 'restore'])->name(
 Route::get('/posts/{post:uuid}/ajax', function (Post $post) {
     return response()->json($post);
 });
+
+require __DIR__ . '/auth.php';
